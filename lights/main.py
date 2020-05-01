2c9c593e405c40e7f5985eaa7ec0c2f2325c3f81
José Manuel Lucas <josemanuel@jmlweb.es>
2020-05-01 22:07:14 +0200

    add blink logic for lights demo

--- lights/main.py
+++ lights/main.py
@@ -3,15 +3,21 @@ from signal import pause
 
 LED_PIN = 18
 BUTTON_PIN = 24
+MODES = ['fixed', 'blink']
 
 led = LED(LED_PIN)
 button = Button(BUTTON_PIN)
+next_mode = MODES[0]
 
 def toggle_led():
   if (led.is_active):
     led.off()
-  else:
+  elif (next_mode == MODES[0]):
+    next_mode = MODES[1]
     led.on()
+  else:
+    next_mode = MODES[0]
+    led.blink(0.5, 0.5)
 
 button.when_pressed = toggle_led
 
