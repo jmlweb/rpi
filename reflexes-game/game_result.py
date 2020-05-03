61491fa2f1c0b118cb9c5057a0fea595b97a68c4
José Manuel Lucas <josemanuel@jmlweb.es>
2020-05-04 00:10:56 +0200

    better orange color

--- reflexes-game/game_result.py
+++ reflexes-game/game_result.py
@@ -1,8 +1,9 @@
+from gpiozero import RGBLED
 from colorzero import Color
 
 
 class GameResult:
-    def __init__(self, result_led):
+    def __init__(self, result_led: RGBLED):
         self.result_led = result_led
 
     def reset(self):
@@ -12,7 +13,7 @@ class GameResult:
         self.result_led.color = Color("red")
 
     def warn(self):
-        self.result_led.color = Color("orange")
+        self.result_led.color = Color("yellow") + Color("red")
 
     def success(self):
         self.result_led.color = Color("green")
