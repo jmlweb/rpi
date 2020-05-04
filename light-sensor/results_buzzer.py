ef4251edac671fdcfaa280a928196048c0424037
José Manuel Lucas <josemanuel@jmlweb.es>
2020-05-05 00:57:02 +0200

    invert frequencies

--- light-sensor/results_buzzer.py
+++ light-sensor/results_buzzer.py
@@ -5,13 +5,12 @@ from gpiozero.tones import Tone
 class ResultsBuzzer:
     def __init__(self, pin):
         self.buzzer = TonalBuzzer(pin)
-        min = Tone(self.buzzer.min_tone).frequency
-        max = Tone(self.buzzer.max_tone).frequency
-        self.conversion = (max - min) / 100
-        self.min = min
+        self.min = Tone(self.buzzer.min_tone).frequency
+        self.max = Tone(self.buzzer.max_tone).frequency
+        self.conversion = (self.max - self.min) / 100
 
     def play(self, light_value):
         # light_value goes from 0 to 100,
         # so we need to convert it first
-        value = round(light_value * self.conversion + self.min)
+        value = round(self.max - light_value * self.conversion)
         self.buzzer.play(Tone(frequency=value))
